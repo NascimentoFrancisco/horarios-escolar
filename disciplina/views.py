d1705cd46044347436267e34edb0483626fd00b0
NascimentoFrancisco <fmilk9049@gmail.com>
2022-07-26 12:50:46 -0300

    Finalizando o CRUD

--- disciplina/views.py
+++ disciplina/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import CreateView, UpdateView
+from django.views.generic import CreateView, UpdateView,ListView, DeleteView
 from disciplina.models import Disciplina
 from django.urls import reverse_lazy
 # Create your views here.
@@ -15,3 +15,27 @@ class Create_disciplina(CreateView):
 
     def form_valid(self, form):
         return super().form_valid(form)
+
+class Update_disciplina(UpdateView):
+    model = Disciplina
+    template_name: str = 'disciplina/create_disciplina.html'
+    fields = ['nome','carga_horaria','aulas_por_semana','curso_disciplina','profesor_disciplina']
+    success_url = reverse_lazy('disciplinas:list_disciplina')
+
+    def form_valid(self, form):
+        return super().form_valid(form)
+
+class List_disciplina(ListView):
+    template_name: str = 'disciplina/list_disciplina.html'
+
+    def get_queryset(self):
+        queryset = Disciplina.objects.all()
+        return queryset
+
+class Delete_disciplina(DeleteView):
+    model =Disciplina
+    template_name = 'disciplina/delete_disciplina.html'
+    success_url = reverse_lazy('disciplinas:list_disciplina')
+
+    def get_success_url(self):
+        return reverse_lazy('disciplinas:list_disciplina')
--- professor/views.py
+++ professor/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import CreateView, UpdateView
+from django.views.generic import CreateView, UpdateView,ListView,DeleteView
 from professor.models import Professor
 from django.urls import reverse_lazy
 # Create your views here.
@@ -17,3 +17,27 @@ class Create_professor(CreateView):
 
     def form_valid(self, form):
         return super().form_valid(form)
+
+class List_professor(ListView):
+    template_name: str = 'professor/list_professor.html'
+
+    def get_queryset(self):
+        queryset = Professor.objects.all()
+        return queryset
+
+class Update_professor(UpdateView):    
+    model =Professor
+    template_name = 'professor/create_professor.html'
+    fields = ['nome','suap','coordenador','disponibilidade_professor','email','telefone']
+    success_url = reverse_lazy('professores:list_professor')
+
+    def form_valid(self, form):
+        return super().form_valid(form)
+
+class Delete_professor(DeleteView):
+    model =Professor
+    template_name = 'professor/delete_professor.html'
+    success_url = reverse_lazy('professores:list_professor')
+
+    def get_success_url(self):
+        return reverse_lazy('professores:list_professor')
